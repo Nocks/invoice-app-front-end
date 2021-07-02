5e1bf608984d43a2562a9335cb3633997419889c
Enock Kwesi Addey <enock.addey@gmail.com>
2021-07-02 23:58:00 +0000

    WIP theme toggler feature

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+const body = document.getElementsByTagName('body');
+const themeToggler = document.querySelector('.theme-toggler');
+
+
+themeToggler.addEventListener('click', () => {
+    console.log('Working...');
+});
