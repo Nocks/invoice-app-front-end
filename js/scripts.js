d7852bac0b997cc8e13f6c919079b0f279b6b94c
Enock Kwesi Addey <enock.addey@gmail.com>
2021-07-06 19:13:51 +0000

    Delay theme toggling

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,12 @@ const themeToggler = document.querySelector('.theme-toggler__img');
 
 
 themeToggler.addEventListener('click', () => {
-
-    pageBody.classList.toggle('dark-theme');
-    
-    if (pageBody.classList.contains("dark-theme")) {
-        themeToggler.src="assets/icon-sun.svg";
-    } else {
-        themeToggler.src="assets/icon-moon.svg";
-    }
-    
+    setTimeout(() => {
+        pageBody.classList.toggle('dark-theme');
+        if (pageBody.classList.contains("dark-theme")) {
+            themeToggler.src="assets/icon-sun.svg";
+        } else {
+            themeToggler.src="assets/icon-moon.svg";
+        }
+    }, 1500);
 });
